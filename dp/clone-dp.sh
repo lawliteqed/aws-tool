a4cdab72c7310a6d47cae99034de2e29c6ed4c50
lawliteqed <lawliteqed@yahoo.co.jp>
2017-10-26 09:06:46 +0000

    dp

--- dp/clone-dp.sh
+++ dp/clone-dp.sh
@@ -3,14 +3,23 @@ PROFILE=$1
 ORG_DP_NAME=$2
 ORG_DP_ID=$(aws --profile ${PROFILE} datapipeline list-pipelines --query "pipelineIdList[?name==\`$ORG_DP_NAME\`].id" --output text)
 NEW_DP_NAME=$3
+NEW_DP_ID=""
+NEW_DP_JSON=conf/$3.json
 
+if [ $# -eq 3 ]; then
+   echo "ok"
+else
+   usage
+   exit 1
+fi
 
 function usage ()
 {
    echo "usage:  $0 <profile> <original-dp-name> <new-dp-name>"
 }
 
-#引数に与えられたDPの存在チェック
+#$1に与えられたDPの存在チェック
+
 function dp_check()
 {
    local check=$(aws --profile ${PROFILE} datapipeline list-pipelines  --query "pipelineIdList[?name==\`$1\`]" --output text)
@@ -24,20 +33,18 @@ function dp_check()
 
 
 
-function get_dp_difenition()
-{
-   aws --profile ${PROFILE} datapipeline get-pipeline-definition --pipeline-id $ORG_DP_ID > conf/${NEW_DP_NAME}.json
-   echo "hoge"
-}
+aws --profile ${PROFILE} datapipeline get-pipeline-definition --pipeline-id $ORG_DP_ID > $NEW_DP_JSON
 
 
-#dp_check $ORG_DP_NAME
-get_dp_difenition
+aws --profile ${PROFILE} datapipeline create-pipeline --name ${NEW_DP_NAME} --unique-id hojoji
 
-if [ $# -eq 3 ]; then
-   echo "ok"
-   exit 0
-else
-   usage
-   exit 1
-fi
+#fixme
+#すでに同名のpipelineが存在するとエラー。コピー先のpipelineがひとつであることを確認
+#同名のpipeline作成はあまりないので問題無いかと。
+NEW_DP_ID=$(aws --profile ${PROFILE} datapipeline list-pipelines --query "pipelineIdList[?name==\`$NEW_DP_NAME\`].id" --output text)
+
+#dpで起動しているEC2インスタンスを区別するためタグ付け
+aws --profile ${PROFILE} datapipeline add-tags --pipeline-id ${NEW_DP_ID} --tags key=datapipeline,value=${NEW_DP_NAME}
+
+
+aws --profile ${PROFILE} datapipeline put-pipeline-definition --pipeline-id ${NEW_DP_ID} --pipeline-definition file://${NEW_DP_JSON}
