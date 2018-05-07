5f80d307958a77251b0f6df4cfec9bdc17601e46
ShotaSakai <lawliteqed@yahoo.co.jp>
2018-05-08 03:13:20 +0900

    jedi-vim

--- ec2/start_ec2.py
+++ ec2/start_ec2.py
@@ -10,7 +10,10 @@ _list_instances = ec2_c.describe_instances()['Reservations']
 for i, _dict_name in enumerate(_list_instances):
     _dict_instances = _dict_name['Instances'][0]
     _instance_id = _dict_instances['InstanceId']
+    _state = _dict_instances['State']['Name']
+#    _tags = parse_keyvalue_sets(instance['Tags'])
     print (_instance_id)
+    print (_state)
 
 
 
