c569bf187dd55fa48514a9360f09803fd67d43e4
lawliteqed <lawliteqed@yahoo.co.jp>
2018-05-11 05:32:10 +0000

    dp read

--- dp/list_pipeline.sh
+++ dp/list_pipeline.sh
@@ -1,3 +1,4 @@
+#/bin/sh
 PROFILE=$1
 
 function usage (){
---
+++ dp/read_pipeline.sh
@@ -0,0 +1,47 @@
+#/bin/sh
+
+PROFILE=$1
+PIPELINEID=$2
+ARGUMENT=2
+
+function usage() {
+  echo "usage : $0 <profile> <pipeline-id>"
+}
+
+function read_pipeline() {
+  local _id=$1
+  local _output_file=$2
+  aws --profile $PROFILE datapipeline get-pipeline-definition \
+      --pipeline-id $_id > $_output_file
+}
+
+function get_pipeline_name(){
+   local _id=$1
+   aws --profile $PROFILE datapipeline describe-pipelines \
+       --pipeline-ids $_id | jq -r '.pipelineDescriptionList[].name'
+}
+
+function file_exists_check() {
+  local _file=$1
+  #ファイルが存在したら処理を終了。
+  if [ -f $_file ]; then
+    echo "$1 is exists!  It suspends processing."
+    exit 2
+  fi
+}
+
+#引数チェック
+if [ $# -ne $ARGUMENT ]; then
+   usage
+   exit 2
+fi
+
+#ファイル名取得
+#pipeline_name=`get_pipeline_name $PIPELINEID`
+#
+#output_file=$pipeline_name.json
+#
+##出力ファイル存在チェック
+#file_exists_check $output_file
+#
+#read_pipeline $PIPELINEID $output_file
