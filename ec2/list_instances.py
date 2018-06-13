9173eddce4c06c5c4b84ddb28f8d198f3b18e981
ShotaSakai <lawliteqed@yahoo.co.jp>
2018-06-14 01:30:29 +0900

    launch instances

--- ec2/list_instances.py
+++ ec2/list_instances.py
@@ -4,8 +4,11 @@ ec2_c = boto3.client('ec2')
 a = ec2_c.describe_instances()
 
 
+イメージとしては
+no,instanceId,status(stop/start/terminated),tagsname,publicDNS,privateIP
 
 
-instanceId = a["Reservations"][2]["Instances"][0]['InstanceId']
+instanceId = a["Reservations"][0]["Instances"][0]['InstanceId']
+
 
 print(instanceId)
