2adc7f6375ff236f6eb4631f160e2aca691359f1
ShotaSakai <lawliteqed@yahoo.co.jp>
2018-06-20 02:10:25 +0900

    launch instances

--- ec2/save.py
+++ ec2/save.py
@@ -1,18 +1,34 @@
 # coding: utf-8
-ec2_r
-get_ipython().magic('pinfo ec2_c.describe_instances')
-ec2_c.describe_instances()
-res = ec2_c.describe_instances()
-res
-res["Reservations"][0]["Instances"][0]['InstanceId']
-res["Reservations"][0]["Instances"][1]['InstanceId']
-res["Reservations"][1]["Instances"][0]['InstanceId']
-res["Reservations"]?
-type(res["Reservations"]?)
-type(res["Reservations"])
-list_res = res["Reservations"]
-for i in list_res:
+instances = ec2_c.describe_instances()
+
+
+#イメージとしては
+#no,instanceId,status(stop/start/terminated),tagsname,publicDNS,privateIP
+
+
+#instanceId = a["Reservations"][0]["Instances"][0]['InstanceId']
+
+list_instances = instances["Reservations"]
+for i in list_instances:
     a = i["Instances"][0]['InstanceId']
+    b = i["Instances"][0]['State']['Name']
+    c = i["Instances"][0]['Tags']
     print(a)
+    print(b)
+    print(c)
     
-get_ipython().magic('save save.py 1-14')
+c
+import pandas
+c
+type(C)
+type(c)
+get_ipython().magic('ll ')
+a =[1,6,9]
+a
+[i * 2 for i in a]
+c
+{k: v for k, v in c}
+c
+d = [x['Value'] for x in c if x['Key'] == 'Name']
+d
+get_ipython().magic('save save.py 1-17')
