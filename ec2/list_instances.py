1afebd45a59eb0eb42be460c5d32115771d6bad9
ShotaSakai <lawliteqed@yahoo.co.jp>
2018-07-19 00:53:12 +0900

    roop

--- ec2/list_instances.py
+++ ec2/list_instances.py
@@ -12,10 +12,14 @@ for i in list_instances:
     a = i["Instances"][0]['InstanceId']
     b = i["Instances"][0]['State']['Name']
     c = i["Instances"][0]['Tags']
+    for j in c:
+        if ("Name" in j.values()):
+            value =j['Value']
+            print(value)
     print(a)
     print(b)
-    print(c)
-    t = type(c)
-    print(t)
+    #print(c)
+    #t = type(c)
+    #print(t)
     
 
