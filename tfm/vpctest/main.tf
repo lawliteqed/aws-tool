fd2a23d8d172f62fbd5c385f5873850661db24ac
shota sakai <lawliteqed@yahoo.co.jp>
2020-01-16 01:32:43 +0000

    trm-vpc

---
+++ tfm/vpctest/main.tf
@@ -0,0 +1,19 @@
+# variable "region" {
+#   default = "ap-northeast-1"
+# }
+
+provider "aws" {
+  region                  = "ap-northeast-1"
+  shared_credentials_file = "/home/ec2-user/.aws/credentials"
+  profile                 = "d"
+}
+
+resource "aws_vpc" "myVPC" {
+  cidr_block           = "10.1.0.0/16"
+  instance_tenancy     = "default"
+  enable_dns_support   = "true"
+  enable_dns_hostnames = "false"
+  tags = {
+    Name = "myVPC"
+  }
+}
